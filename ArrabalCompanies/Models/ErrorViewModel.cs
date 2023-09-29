4e7035c46df6c8fcf1eb2eb6479ee728bf0a9982
ClaudioLenci <73424774+ClaudioLenci@users.noreply.github.com>
2023-09-29 11:17:54 +0200

    Aggiungere i file di progetto.

---
+++ ArrabalCompanies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArrabalCompanies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
