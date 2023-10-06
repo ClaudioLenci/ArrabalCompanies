05c5c4c814d4a55655d7ddad865516ab9ca7b778
ClaudioLenci <73424774+ClaudioLenci@users.noreply.github.com>
2023-10-06 11:31:59 +0200

    #1 prima implementazione gestione account (login, signup, ecc)

--- ArrabalCompanies/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ArrabalCompanies/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -70,6 +70,14 @@ namespace ArrabalCompanies.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [Display(Name = "Name")]
+            public string Firstname { get; set; }
+
+            [Required]
+            [Display(Name = "Surname")]
+            public string Lastname { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
